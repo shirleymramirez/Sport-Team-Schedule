ae485273f2792a23ce9783a4802b268dd89958b3
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-04-02 17:52:21 -0700

    added login and password forms in a card

--- sport-team-scheduler/client/src/pages/Login/Login.js
+++ sport-team-scheduler/client/src/pages/Login/Login.js
@@ -1,6 +1,59 @@
 import React from "react";
+// allows for text input use 
+import { TextField, TextFieldIcon, TextFieldHelperText } from 'rmwc/TextField';
+// allows for card use 
+import {
+  Card,
+  CardPrimaryAction,
+  CardMedia,
+  CardAction,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons
+} from 'rmwc/Card';
+import { Typography } from 'rmwc/Typography';
+import { Grid, GridCell } from 'rmwc/Grid';
 
-const Login = () =>
-  <div>Login and SignUp page</div>;
+
+const Login = () =>(
+<div>
+<Grid>
+ <GridCell span="4"></GridCell>
+ <GridCell span="4">
+	<Card style={{width: '21rem'}}>
+	  <CardPrimaryAction>
+	    <CardMedia sixteenByNine style={{backgroundImage: 'url(https://material-components-web.appspot.com/images/16-9.jpg)'}}/>
+	    <div style={{padding: '0 1rem 1rem 1rem'}}>
+	      <Typography use="title" tag="h2">Login To Sport-Team-Schedule</Typography>
+	      <Typography
+	        use="subheading1"
+	        tag="h3"
+	        theme="text-secondary-on-background"
+	        style={{marginTop: '-1rem'}}
+	      >
+			<TextField label="UserName" />
+	      </Typography>
+	      <Typography use="body1" tag="div" theme="text-secondary-on-background">
+		<TextField label="PassWord" />
+	      </Typography>
+	    </div>
+	  </CardPrimaryAction>
+	  <CardActions>
+	    <CardActionButtons>
+	      <CardAction>Login</CardAction>
+	      <Typography>Don't an account...
+	      	<span>
+	      	<CardAction>Sign in</CardAction>
+			</span>
+		   </Typography>
+	    </CardActionButtons>
+	  </CardActions>
+	</Card>
+ </GridCell>
+ <GridCell span="4"></GridCell>
+</Grid>
+</div>
+
+);
 
 export default Login;
