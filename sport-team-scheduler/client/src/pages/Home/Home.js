105b95da3b9ece7b9c192cd2760d2e77853fbf0b
Kassi Erickson <you@example.com>
2018-04-04 19:17:02 -0700

    created buttons for home page

--- sport-team-scheduler/client/src/pages/Home/Home.js
+++ sport-team-scheduler/client/src/pages/Home/Home.js
@@ -1,5 +1,32 @@
 import React from "react";
 
-const Home = () => <div> This page is for home page. This is where the user can select between coach or parent</div>;
+import { Button, ButtonIcon } from 'rmwc/Button';
+
+import { Grid, GridCell } from 'rmwc/Grid';
+
+import { Typography } from 'rmwc/Typography';
+
+
+const Home = () => 
+<div> 
+
+
+<Typography use="title" tag="h2"><center>I am a...</center></Typography>
+
+<Grid>
+  <GridCell span="4"></GridCell>
+  <GridCell span="4">
+  <center>
+  	<Button stroked className="shirley" ><h2>Parent</h2></Button>
+  	// create a CSS to style the buttons
+  	<Button stroked><h2>Coach</h2></Button>
+  </center>
+  </GridCell>
+  <GridCell span="4"></GridCell>
+</Grid>
+
+
+
+</div>;
 
 export default Home;
