beb5f16d2cdf0be7af071d239c19870ef3491e22
kooldrmony <derrickreyes.23@gmail.com>
2018-04-06 09:15:36 -0700

    added the create roster and create schedule components as well as updated coachview js

---
+++ sport-team-scheduler/client/src/components/Messages/Messages.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Messages.css";
+
+const Messages = () => (
+
+	    <section id="messages">
+
+       <h1 id="message">Messages</h1>
+
+        <p class="p1">Derrick</p>
+
+        <p class="p1">Bryant</p>
+
+        <p class="p1">Kassi</p>
+
+        <p class="p1">Shirley</p>
+      
+    </section>
+  
+     
+    );
+
+
+export default Messages;
---
+++ sport-team-scheduler/client/src/components/Schedule/Schedule.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Schedule.css";
+
+const Schedule = () => (
+
+	<form>
+            
+    <button id="add-schedule" type="submit">Create Schedule</button>
+
+ 	</form>
+  
+     
+    );
+
+
+export default Schedule;
