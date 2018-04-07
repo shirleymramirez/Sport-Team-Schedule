025455141812c29118e42a747073046b20b54411
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-04-07 12:10:09 -0700

    change component messages

--- sport-team-scheduler/client/src/pages/CoachView/CoachView.js
+++ sport-team-scheduler/client/src/pages/CoachView/CoachView.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Roster from "./../../components/Roster";
-// import { Messages } from "./../../components/Messages";
-import SideBar from "../../components/SideBar";
+import CoachViewMessages from "./../../components/CoachViewMessages";
 import Schedule from "./../../components/Schedule";
 
 const CoachView = () =>
   <div>
     <Roster />
     <Schedule />
-    <SideBar />
+    <CoachViewMessages />
   </div>;
 
 export default CoachView;
