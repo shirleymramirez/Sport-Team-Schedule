02b82c92806f69cb7e1b8be661a1c39b52260488
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-04-09 15:10:53 -0700

    set up server for passport, express val, express session, and flash

--- sport-team-scheduler/server.js
+++ sport-team-scheduler/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -13,6 +20,7 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 // Serve up static assets
 app.use(express.static("client/build"));
 // Add routes, both API and view
@@ -22,10 +30,50 @@ app.use(routes);
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/sport-team-scheduler"
 );
+var db = mongoose.connection;
 
 io.on('connection, SocketManager');
 server.listen(PORT, () => {
   console.log("Connected to port:" + PORT);
+});
+
+// express session 
+app.use(session({
+	secret:'secret',
+	saveUninitialized: true,
+	resave:true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// connect flash 
+app.use(flash());
+// var for flash 
+app.use(function(req, res, next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 
