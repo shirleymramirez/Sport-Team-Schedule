0c7b75ab5185f932d3dc30862d4ab433265a7aba
Kassi Erickson <you@example.com>
2018-04-09 19:48:16 -0700

    cleaned up Home page and got links

--- sport-team-scheduler/client/src/App.js
+++ sport-team-scheduler/client/src/App.js
@@ -17,6 +17,7 @@ const App = () =>
       <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/home/login" component={Login} />
         <Route exact path="/chat" component={Chat} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/roster" component={Roster} />
--- sport-team-scheduler/client/src/pages/Home/Home.js
+++ sport-team-scheduler/client/src/pages/Home/Home.js
@@ -1,20 +1,32 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+
 import { Button, ButtonIcon } from 'rmwc/Button';
 import { Grid, GridCell } from 'rmwc/Grid';
 import { Typography } from 'rmwc/Typography';
 import './Home.css';
+import soccerBall from "./images/soccer2.jpg"
 
 
 const Home = () => 
 <div> 
+
+  <div className="main-container">
+    <div className="jumbotron">
+      <center><h1>Sports Team Scheduler!</h1></center>
+      	<img className="soccerBall" src={soccerBall} width="100%" height="100%" />
+
+    </div>
+  </div>
+
 <Typography use="title" tag="h2"><center>I am a...</center></Typography>
 
 	<Grid>
 	  <GridCell span="4"></GridCell>
 	  <GridCell span="4">
 	  <center>
-	  	<Button stroked className="btn1" onClick={this.parentSubmit}><h2>Parent</h2></Button>
-	  	<Button stroked className="btn2"><h2>Coach</h2></Button>
+	  	<Button stroked id="btn1"className={window.location.pathname === "/login" ? "active" : ""}><Link className="btn1" to="/login">Parent</Link></Button>
+	  	<Button stroked id="btn2" className={window.location.pathname === "/login" ? "active" : ""}><Link className="btn2" to="/login">Coach</Link></Button>
 	  </center>
 	  </GridCell>
 	  <GridCell span="4"></GridCell>
