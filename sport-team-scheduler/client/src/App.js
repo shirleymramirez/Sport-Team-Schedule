40f8bdec97ca48113a44e7f741ad3f03aa376194
kooldrmony <derrickreyes.23@gmail.com>
2018-04-13 16:41:20 -0700

    added map and database

--- sport-team-scheduler/client/src/App.js
+++ sport-team-scheduler/client/src/App.js
@@ -8,6 +8,8 @@ import Roster from "./pages/Roster";
 import CoachView from "./pages/CoachView";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+// import Weather from "./components/Weather"
+import MyMapComponent from "./components/Map"
 import "./index.css";
 
 
@@ -23,6 +25,7 @@ const App = () =>
         <Route exact path="/roster" component={Roster} />
         <Route exact path="/login/signin" component={Signin} />
         <Route exact path="/coach" component={CoachView} />
+        <Route exact path="/weather" component={MyMapComponent} />
         <Route component={Home} />
       </Switch>
       <Footer />
