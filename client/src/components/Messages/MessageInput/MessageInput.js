3b00dbc6c355247c512b2d3c25345ba410252806
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-04-14 13:37:27 -0700

    heroku fix

--- client/src/components/Messages/MessageInput/MessageInput.js
+++ client/src/components/Messages/MessageInput/MessageInput.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./MessageInput.css";
 import TextField from "rmwc/TextField";
 
-const MessageInput = props => {
-  return <div className="message-input">
+const MessageInput = props => 
+  <div className="message-input">
       <TextField className="chat-messagesInput" 
         textarea fullwidth label="Type a message..." 
         rows="2" value={props.value}
         onChange={props.onChange}
         onKeyPress={props.onKeyPress} />
     </div>;
-}
 
 export default MessageInput; 
 
