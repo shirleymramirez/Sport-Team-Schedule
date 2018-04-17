ed0f6ce6045b7ea5ab7e2ac761ca57e5cbb72ad4
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-04-17 11:31:56 -0700

    removed some unused tools and cleaned up code a lil

--- sport-team-scheduler/client/src/pages/Roster/Roster.js
+++ sport-team-scheduler/client/src/pages/Roster/Roster.js
@@ -2,32 +2,19 @@ import React from "react";
 import './roster.css';
 import ViewRoster from "./../../components/ViewRoster";
 import { Grid, GridCell } from 'rmwc/Grid';
-import { Typography } from 'rmwc/Typography';
-import { Button, ButtonIcon } from 'rmwc/Button';
-import { TextField, TextFieldIcon, TextFieldHelperText } from 'rmwc/TextField';
-import {
-  Card,
-  CardPrimaryAction,
-  CardMedia,
-  CardAction,
-  CardMediaContent,
-  CardActions,
-  CardActionButtons,
-  CardActionIcons
-} from 'rmwc/Card';
 
 const Roster = () => (
-<div> 
-	<Grid>
+  <div> 
+  	<Grid>
 
-	  <GridCell span="4"></GridCell>
+  	  <GridCell span="4"></GridCell>
 
-		<GridCell span="4">
-			<ViewRoster />
-		</GridCell>
+  		<GridCell span="4">
+  			<ViewRoster />
+  		</GridCell>
 
-	<GridCell span="4"></GridCell>
-	</Grid>
-</div>
-)
+  	<GridCell span="4"></GridCell>
+  	</Grid>
+  </div>
+);
 export default Roster;
