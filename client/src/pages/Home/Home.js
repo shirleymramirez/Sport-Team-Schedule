a6104cb084948bade45325b04a95ee551bbeac35
Kassi Erickson <you@example.com>
2018-04-16 20:21:17 -0700

    fixed Gridcell

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import { Button } from 'rmwc/Button';
-import { Grid, GridCell } from 'rmwc/Grid';
 import { Typography } from 'rmwc/Typography';
 import './Home.css';
 import soccerBall from "./images/soccer2.jpg"
