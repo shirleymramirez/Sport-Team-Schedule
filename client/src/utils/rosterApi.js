3fcc40c4f267eb862d72fa68f2c434d95e567e58
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-04-18 09:55:13 -0700

    fxxed my code

--- client/src/utils/rosterApi.js
+++ client/src/utils/rosterApi.js
@@ -4,7 +4,7 @@ export default {
 	Add: function(data) {
 		console.log(data);
 		// console.log("hello");s
-		return axios.post("/api/add/",{
+		return axios.post("/api/add",{
 			name: data.name,
 			age: data.age,
 			team: data.team
