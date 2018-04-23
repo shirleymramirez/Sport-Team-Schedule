7b4f8140b11e95dcbe59ccbc2222f6c6c8f63081
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-04-22 19:36:45 -0700

    added key for google map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,26 +1,42 @@
 import React from "react";
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div className="map" style={{ height: `300px`, width: '300px' }} />,
-    containerElement: <div className="map" style={{ height: `300px`, width: '300px', position: 'absolute', top: '300px', left: '30px' }} />,
-    mapElement: <div className="map" style={{ height: `300px`, width: '520px' }} />,
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBsJqZjSI4-hSdDrJbF_YKv4pYDzVArAhY&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: (
+      <div className="map" style={{ height: `300px`, width: "300px" }} />
+    ),
+    containerElement: (
+      <div
+        className="map"
+        style={{
+          height: `300px`,
+          width: "300px",
+          position: "absolute",
+          top: "300px",
+          left: "30px"
+        }}
+      />
+    ),
+    mapElement: (
+      <div className="map" style={{ height: `300px`, width: "520px" }} />
+    )
   }),
   withScriptjs,
   withGoogleMap
-)((props) =>(
+)(props =>
   <GoogleMap
     defaultZoom={13}
     defaultCenter={{ lat: 33.415184, lng: -111.831472 }}
   >
-  <Marker 
-  position={{ lat: 33.415184, lng: -111.831472 }} 
-  />
+    <Marker position={{ lat: 33.415184, lng: -111.831472 }} />
   </GoogleMap>
-));
-
-
+);
 export default MyMapComponent;
